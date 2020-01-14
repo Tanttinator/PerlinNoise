f72bcd2d22d9ea19632f8e9a2a85363f8cd1c940
Antti Kangas <antti.kangas99@gmail.com>
2020-01-14 20:12:37 +0200

    Add random offset to sampling

--- Generator.cs
+++ Generator.cs
@@ -11,18 +11,26 @@ namespace PerlinNoise
          * args:
          *  int width: X size of the array
          *  int height: Y size of the array
+         *  int seed: Seed for RNG
          */
-        public static float[,] GenerateHeightmap(int width, int height)
+        public static float[,] GenerateHeightmap(int width, int height, int seed)
         {
             //Initialize the heightmap
             float[,] heightmap = new float[width, height];
 
+            //Initialize RNG
+            System.Random prng = new System.Random(seed);
+
+            //Offset the coordinates by a random value
+            float offsetX = prng.Next(-100000, 100000);
+            float offsetY = prng.Next(-100000, 100000);
+
             //Loop through our heightmap and generate noise values for each element
-            for(int x = 0; x < width; x++)
+            for (int x = 0; x < width; x++)
             {
                 for(int y = 0; y < height; y++)
                 {
-                    heightmap[x, y] = Mathf.PerlinNoise((float)x / 100f, (float)y / 100f);
+                    heightmap[x, y] = Mathf.PerlinNoise(((float)x + offsetX) / 100f, ((float)y + offsetY) / 100f);
                 }
             }
 
