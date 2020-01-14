215435eeb15c636762a2c32e03a5f6196a086538
Antti Kangas <antti.kangas99@gmail.com>
2020-01-14 19:28:00 +0200

    Add Generator.GenerateHeightmap

---
+++ Generator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PerlinNoise
+{
+    //Base class for perlin noise generation
+    public class Generator
+    {
+        /**Generates a 2D array of noise values
+         * args:
+         *  int width: X size of the array
+         *  int height: Y size of the array
+         */
+        public static float[,] GenerateHeightmap(int width, int height)
+        {
+            //Initialize the heightmap
+            float[,] heightmap = new float[width, height];
+
+            //Loop through our heightmap and generate noise values for each element
+            for(int x = 0; x < width; x++)
+            {
+                for(int y = 0; y < height; y++)
+                {
+                    heightmap[x, y] = 1f;
+                }
+            }
+
+            return heightmap;
+        }
+    }
+}
